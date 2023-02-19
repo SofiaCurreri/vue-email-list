c29543052a940dae1094b254357ff4edeb6cd8c0
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-19 18:04:55 +0100

    error 403

--- js/app.js
+++ js/app.js
@@ -2,11 +2,28 @@
 //* https://flynn.boolean.careers/exercises/api/random/mail
 //* generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      emails: [],
+    };
+  },
+
+  methods: {
+    generateMail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((result) => {
+            this.emails.push(result.data.response);
+          });
+      }
+    },
   },
 });
 
