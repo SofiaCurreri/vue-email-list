668f5a9df03119d8e1e1440c4bb85dd8c4aac1df
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-19 17:01:21 +0100

    file js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//* Attraverso l'apposita API di Boolean
+//* https://flynn.boolean.careers/exercises/api/random/mail
+//* generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {};
+  },
+});
+
+app.mount("#app");
